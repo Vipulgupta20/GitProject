814b1957118e51b3753158d5d260f292fc511823
Vipul.Gupta <Vipul.Gupta@ZMBLR1627>
2022-01-06 16:34:41 +0530

    done additional conditions

--- src/gitpackage/Test2.java
+++ src/gitpackage/Test2.java
@@ -3,7 +3,7 @@ package gitpackage;
 public class Test2 {
 	public static void main(String[] args) {
 		String hungry = "yes";
-		if (hungry == "yes")
+		if (hungry == "yes"|| hungry=="y")
 		System.out.println("eat something....");
 		else {
 		System.out.println("Do work.......");
